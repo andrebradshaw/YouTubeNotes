f0a775aecca7b240c5532b15d0ad40d2f5baf616
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-11-04 00:08:45 -0400

    Update saveAtPaused.js

--- saveAtPaused.js
+++ saveAtPaused.js
@@ -6,26 +6,27 @@ var duration =   	document.getElementsByClassName("ytp-time-duration");
 
 var timestamp = current[0].innerText;
 
-var timehours = /^\d+:\d+:\d+$/.exec(timestamp);
-var timemins = /^\d+:\d+$/.exec(timestamp);
+function convertTime2Secs(str){
+	var timehours = /^\d+:\d+:\d+$/.exec(str);
+	var timemins = /^\d+:\d+$/.exec(str);
 
-var hour; if(timehours != null) hour = parseInt(/^\d+(?=:)/.exec(timehours[0])[0]);
-var mins; if(timehours != null) mins = parseInt(/(?<=\d+:)\d+(?=:)/.exec(timehours[0])[0]);
-var secs; if(timehours != null) secs = parseInt(/\d+$/.exec(timehours[0])[0]);
+	var hour; if(timehours != null) hour = parseInt(/^\d+(?=:)/.exec(timehours[0])[0]);
+	var mins; if(timehours != null) mins = parseInt(/(?<=\d+:)\d+(?=:)/.exec(timehours[0])[0]);
+	var secs; if(timehours != null) secs = parseInt(/\d+$/.exec(timehours[0])[0]);
 
-var hour; if(timehours == null) hour = 0;
-var mins; if(timemins != null) mins = parseInt(/^\d+(?=:)/.exec(timemins[0])[0]);
-var secs; if(timemins != null) secs = parseInt(/\d+$/.exec(timemins[0])[0]);
+	var hour; if(timehours == null) hour = 0;
+	var mins; if(timemins != null) mins = parseInt(/^\d+(?=:)/.exec(timemins[0])[0]);
+	var secs; if(timemins != null) secs = parseInt(/\d+$/.exec(timemins[0])[0]);
 
-var startFrom = '?t='+((hour*(60*60))+(mins*60)+(secs));
-var vidId = '&id='+/watch?v=/.exec(window.location.href)[0];
+	return ((hour*(60*60))+(mins*60)+(secs));
+}
+
+var startFrom = '?t='+convertTime2Secs(timestamp);
+var vidId = /(?<=watch\?v=)\w+/.exec(window.location.href)[0];
 
 var webAppUrl = "";
 var output = webAppUrl+startFrom+vidId;
-//402
-console.log(output); //hours+mins+secs)
 
-
-//?t=332
+console.log(output); 
 
 
