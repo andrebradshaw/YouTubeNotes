4eaf2332e103ca822ce21960dd8cc0583a211d6e
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-12-21 16:29:46 -0500

    Update openTranscript.js

--- openTranscript.js
+++ openTranscript.js
@@ -1,3 +1,25 @@
+
+function clickMenu(){
+	document.getElementById('menu-container').getElementsByTagName('ytd-menu-renderer')[0].getElementsByTagName('yt-icon')[4].click()
+}
+
+function clickTrans(){
+	setTimeout(()=>{
+		document.getElementsByTagName('paper-listbox')[1].getElementsByTagName('ytd-menu-service-item-renderer')[1].click();
+	},1600);
+}
+
+function getTrans(){
+	setTimeout(()=>{
+		document.getElementById('transcript').getElementsByClassName('cue-group style-scope ytd-transcript-body-renderer').length;
+    },2400);
+}
+
+var first = new Promise(res=>{res(clickMenu())});
+
+first.then(clickTrans()).then(getTrans())
+
+/*
 function openTranscript(){
 	function c1(){
 		setTimeout(() =>{
@@ -9,3 +31,4 @@ function openTranscript(){
 	
 	c.then(document.getElementsByClassName("style-scope ytd-menu-popup-renderer")[2].click());	
 }
+*/
