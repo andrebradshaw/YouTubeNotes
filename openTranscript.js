62c270b0b713e4810fe3eae55186e1182448027c
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-12-23 11:41:07 -0500

    Update openTranscript.js

--- openTranscript.js
+++ openTranscript.js
@@ -1,18 +1,18 @@
-
 function clickMenu(){
 	document.getElementById('menu-container').getElementsByTagName('ytd-menu-renderer')[0].getElementsByTagName('yt-icon')[4].click()
 }
 
 function clickTrans(){
 	setTimeout(()=>{
-		document.getElementsByTagName('paper-listbox')[1].getElementsByTagName('ytd-menu-service-item-renderer')[1].click();
-	},1600);
+		var paperBox = document.getElementsByTagName('paper-listbox');
+		paperBox[paperBox.length-1].getElementsByTagName('ytd-menu-service-item-renderer')[1].click();
+	},1590);
 }
 
 function getTrans(){
 	setTimeout(()=>{
 		document.getElementById('transcript').getElementsByClassName('cue-group style-scope ytd-transcript-body-renderer').length;
-    },2400);
+    },2100);
 }
 
 var first = new Promise(res=>{res(clickMenu())});
